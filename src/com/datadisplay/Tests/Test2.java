cab961a32e715591fb662e015aa2090ce2d90f28
Daniel Nichols <dando18studios@gmail.com>
2016-03-12 12:52:38 -0500

    Included bar chart in example

--- src/com/datadisplay/Tests/Test2.java
+++ src/com/datadisplay/Tests/Test2.java
@@ -1,5 +1,8 @@
 package com.datadisplay.Tests;
 
+import java.util.Random;
+
+import com.datadisplay.BarChart;
 import com.datadisplay.CartesianGraph;
 import com.datadisplay.DataDisplay;
 
@@ -14,13 +17,41 @@ public class Test2 {
 	@SuppressWarnings("unused")
 	public static void main(String[] args){
 		
+		Random rand = new Random(System.nanoTime());
+		
 		// the frame will split into 4 panels
 		DataDisplay dd = new DataDisplay(4);
 		
 		CartesianGraph cg0 = dd.showCartesian();
+		cg0.setConnectPoints(true);
+		for(int i=-4; i<5; i+=1){
+			cg0.plot(i, rand.nextInt(5));
+		}
+		cg0.showMean();
+		cg0.showStandardDeviation();
+		
+		BarChart bc0 = dd.showBarChart();
+		for(int i=0; i<5; i++){
+			bc0.addValue(rand.nextInt(5)+1);
+		}
+		
+		BarChart bc1 = dd.showBarChart();
+		int bc1_max = rand.nextInt(8-3)+3;
+		for(int i=0; i<bc1_max; i++){
+			bc1.addValue(rand.nextInt(5)+1);
+		}
+		
 		CartesianGraph cg1 = dd.showCartesian();
-		CartesianGraph cg2 = dd.showCartesian();
-		CartesianGraph cg3 = dd.showCartesian();
+		cg1.setConnectPoints(true);
+		for(int i=-4; i<5; i+=1){
+			cg1.plot(i, rand.nextInt(3)* ((rand.nextBoolean()) ? 1 : -1));
+		}
+		cg1.showMean();
+		cg1.showStandardDeviation();
+		
+		
+		
+		
 	}
 	
 }
