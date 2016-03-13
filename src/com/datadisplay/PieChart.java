fe49c7b2cb46c6a3831c4e9e085dcaffd78f5946
Daniel Nichols <dando18studios@gmail.com>
2016-03-13 17:14:34 -0400

    title and key added

--- src/com/datadisplay/PieChart.java
+++ src/com/datadisplay/PieChart.java
@@ -1,8 +1,10 @@
 package com.datadisplay;
 
 import java.awt.Color;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,17 +14,22 @@ public class PieChart extends JPanel{
 	private static final long serialVersionUID = 1L;
 	
 	private int cushion = 20;
+	private int key_square = 10;
 	
-	private Color[] pie_colors = {Color.RED,Color.BLUE,Color.GREEN,Color.ORANGE,Color.PINK};
+	private Color[] pie_colors = {Color.RED,Color.BLUE,Color.GREEN,Color.ORANGE,Color.PINK,Color.MAGENTA};
 	private int color_inc = 0;
 	
 	private List<Double> percentages;
+	private List<String> labels;
+	private int precision=2;
+	private String title = "";
 	
 	public PieChart(){
 		
 		this.setBackground(Color.WHITE);
 		
 		percentages = new ArrayList<Double>();
+		labels = new ArrayList<String>();
 		
 	}
 	
@@ -30,6 +37,7 @@ public class PieChart extends JPanel{
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 		
 		drawCircle(g2d);
 		
@@ -37,6 +45,7 @@ public class PieChart extends JPanel{
 	
 	private void drawCircle(Graphics2D g2d){
 		int diameter = ( (this.getWidth()<this.getHeight()) ? (this.getWidth()-cushion*2) : (this.getHeight()-cushion*2) );
+		FontMetrics fm = g2d.getFontMetrics();
 		
 		int degree = 0;
 		for(int i=0; i<percentages.size(); i++){
@@ -45,18 +54,44 @@ public class PieChart extends JPanel{
 			g2d.fillArc((this.getWidth()-diameter)/2, (this.getHeight()-diameter)/2, diameter, diameter, 
 					degree, (int)(360*percentages.get(i)));
 			degree += 360*percentages.get(i);
+			g2d.fillRect(this.getWidth()/2+diameter/2+cushion, (this.getHeight()/2-diameter/2)+i*fm.getHeight()+cushion/2, 
+					key_square, key_square);
+			g2d.drawString(labels.get(i)+" - "+MathUtilities.round(percentages.get(i)*100.0, precision)+"%", 
+					this.getWidth()/2+diameter/2+cushion*2, (this.getHeight()/2-diameter/2)+i*fm.getHeight()+cushion/2+key_square-1);
 		}
 		color_inc=0;
 		
 		g2d.setColor(Color.BLACK);
 		g2d.drawOval((this.getWidth()-diameter)/2, (this.getHeight()-diameter)/2, diameter, diameter);
+		
+		g2d.drawString(title, cushion/2, cushion/2+fm.getHeight());
 	}
 	
-	public void addPercentage(double percentage){
+	public void addValue(String label, double percentage){
+		labels.add(label);
 		percentages.add(percentage);
 		if(MathUtilities.sum( percentages ) > 1){
 			throw new IllegalArgumentException("sum of values should be <= 1");
 		}
 	}
 	
+	public void addValue(double percentage){
+		addValue("",percentage);
+	}
+	
+	public void setPrecision(int precision){
+		this.precision = precision;
+	}
+	
+	public int getPrecision(){
+		return precision;
+	}
+	
+	public void setTitle(String title){
+		this.title = title;
+	}
+	
+	public String getTitle(){
+		return title;
+	}
 }
