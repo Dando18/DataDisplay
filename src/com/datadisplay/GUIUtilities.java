02fdeee34af2174ad217a541657a9f9cb4d6866f
Daniel Nichols <dando18studios@gmail.com>
2016-03-13 08:47:24 -0400

    new randColors

--- src/com/datadisplay/GUIUtilities.java
+++ src/com/datadisplay/GUIUtilities.java
@@ -1,9 +1,16 @@
 package com.datadisplay;
 
+import java.awt.Color;
+import java.util.Random;
+
 public class GUIUtilities {
 	
 	private GUIUtilities(){}
 	
-	
+	public static Color randomColor(){
+		Random rand = new Random(System.nanoTime());
+		float[] hsb = Color.RGBtoHSB(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256), null);
+		return Color.getHSBColor(hsb[0], hsb[1], hsb[2]);
+	}
 	
 }
