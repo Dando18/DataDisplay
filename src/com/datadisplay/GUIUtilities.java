33bec504af724566f9a89383ca61e7936869906b
Daniel Nichols <dando18studios@gmail.com>
2016-05-15 16:56:15 -0400

    Graphics2D multi-line text drawing

--- src/com/datadisplay/GUIUtilities.java
+++ src/com/datadisplay/GUIUtilities.java
@@ -1,6 +1,7 @@
 package com.datadisplay;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
 import java.util.Random;
 
 public class GUIUtilities {
@@ -11,6 +12,11 @@ public class GUIUtilities {
 		Random rand = new Random(System.nanoTime());
 		float[] hsb = Color.RGBtoHSB(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256), null);
 		return Color.getHSBColor(hsb[0], hsb[1], hsb[2]);
+	}
+	
+	public static void drawString(Graphics2D g, String text, int x, int y) {
+	    for (String line : text.split("\n"))
+	        g.drawString(line, x, y += g.getFontMetrics().getHeight());
 	}
 	
 }
