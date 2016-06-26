7facb7a52e4caf3c5ad3d903cf87b7c9bb1a4cbe
Daniel Nichols <dando18studios@gmail.com>
2016-06-26 13:21:56 -0400

    string to color method added

--- src/com/datadisplay/console/ConsoleUtilities.java
+++ src/com/datadisplay/console/ConsoleUtilities.java
@@ -1,10 +1,20 @@
 package com.datadisplay.console;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+import com.datadisplay.GUIUtilities;
+import com.fathzer.soft.javaluator.DoubleEvaluator;
 
 public class ConsoleUtilities {
 
@@ -26,6 +36,11 @@ public class ConsoleUtilities {
 		return nums;
 	}
 	
+	public static Double eval(String in){
+		if(in == null || in.trim().equals("")) return null;
+		return new DoubleEvaluator().evaluate(in);
+	}
+	
 	public static <T> String listToString(List<T> l){
 		String response = "[";
 		for (int i = 0; i < l.size() - 1; i++) {
@@ -38,14 +53,14 @@ public class ConsoleUtilities {
 	public static String replaceVarWithValue(String input, Map<String, Double> vars) {
 		String result = "";
 
-		Pattern p = Pattern.compile("\\s*((\\d*(\\.?\\d+)?)|([\\+\\-\\*/]))\\s*");
-		Matcher m = p.matcher(input);
-		while (m.find()) {
-			String term = m.group();
-			if (vars.containsKey(term)) {
-				term = vars.get(term).toString();
+		String[] terms = input.split(String.format("((?<=%1$s)|(?=%1$s))", "\\s*\\)?[\\+\\-\\*/]\\(?\\s*"));
+		
+		for(int i=0; i<terms.length; i++){
+			if(vars.containsKey(terms[i])){
+				result += ""+vars.get(terms[i]);
+			}else{
+				result += terms[i];
 			}
-			result += term;
 		}
 
 		return result;
@@ -65,6 +80,75 @@ public class ConsoleUtilities {
 			}
 		}
 		return args;
+	}
+	
+	public static Color stringToColor(String in){
+		if(in.startsWith("#")){
+			return new Color( Integer.valueOf( in.substring(1,3), 16),
+							  Integer.valueOf( in.substring(3,5), 16),
+							  Integer.valueOf( in.substring(5,7), 16));
+		}else if(Pattern.matches("\\d{1,3},\\d{1,3},\\d{1,3}", in)){
+			String[] nums = in.split(",");
+			return new Color( Integer.parseInt(nums[0]),
+							  Integer.parseInt(nums[1]),
+							  Integer.parseInt(nums[2]));
+		}else{
+			switch(in.toLowerCase()){
+			case "black":
+				return Color.BLACK;
+			case "white":
+				return Color.WHITE;
+			case "blue":
+				return Color.BLUE;
+			case "red":
+				return Color.RED;
+			case "yellow":
+				return Color.YELLOW;
+			case "gray":
+				return Color.GRAY;
+			case "grey":
+				return Color.GRAY;
+			case "orange":
+				return Color.ORANGE;
+			case "purple":
+				return Color.MAGENTA;
+			case "magenta":
+				return Color.MAGENTA;
+			case "light gray":
+				return Color.LIGHT_GRAY;
+			case "light grey":
+				return Color.LIGHT_GRAY;
+			case "dark gray":
+				return Color.DARK_GRAY;
+			case "dark grey":
+				return Color.DARK_GRAY;
+			case "cyan":
+				return Color.CYAN;
+			default:
+				return Color.WHITE;
+			}
+		}
+		
+	}
+	
+	public static boolean dump(String file_name, JPanel p){
+		try{
+			if(file_name.endsWith("/") || "".equals(file_name)){
+				file_name += String.format("piechart_%X", (long)(Math.random()*1000));
+			}
+			if(!file_name.endsWith(".png")){
+				file_name += ".png";
+			}
+			File f = new File(file_name);
+			f.getParentFile().mkdirs();
+			f.createNewFile();
+			BufferedImage bi = GUIUtilities.createImage(p);
+			ImageIO.write(bi, "png", f);
+		} catch(IOException e){
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 
 }
