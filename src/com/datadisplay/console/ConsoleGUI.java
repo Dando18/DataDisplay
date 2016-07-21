5c35e2e5902f4cea3903e45075c2c4c6395a183b
Daniel Nichols <dando18studios@gmail.com>
2016-07-21 14:53:57 -0400

    fixed quotations in the middle of the command issue

--- src/com/datadisplay/console/ConsoleGUI.java
+++ src/com/datadisplay/console/ConsoleGUI.java
@@ -2,6 +2,7 @@ package com.datadisplay.console;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FileDialog;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
@@ -9,24 +10,39 @@ import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
 
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
+import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.colorchooser.AbstractColorChooserPanel;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
@@ -52,6 +68,11 @@ public class ConsoleGUI extends JFrame {
 	private JTextPane cons;
 	private JTextField inp;
 
+	// menu
+	private JMenuBar mbar;
+	private JMenu run;
+	private JMenuItem open_and_run;
+
 	// attributes
 	private Font font;
 	private Color font_color = Color.BLACK;
@@ -64,6 +85,9 @@ public class ConsoleGUI extends JFrame {
 	// suggesting
 	private boolean suggesting = false;
 	private String suggested = "";
+
+	// master mode opposed to normal mode
+	public boolean master = false;
 
 	public ConsoleGUI() {
 		super();
@@ -101,8 +125,11 @@ public class ConsoleGUI extends JFrame {
 				super.paint(g);
 				Graphics2D g2d = (Graphics2D) g.create();
 				if (!"".equals(inp.getText())) {
-					for (String key : ci.commands.keySet()) {
-						if (key.startsWith(inp.getText()) && !key.equals(inp.getText())) {
+					List<String> keys = new ArrayList<String>(ci.commands.keySet());
+					Collections.sort(keys);
+					for (String key : keys) {
+						if (key.startsWith(inp.getText()) && !key.equals(inp.getText())
+								&& !keys.contains(inp.getText())) {
 							try {
 								int pos = inp.getCaretPosition();
 								Rectangle loc = inp.modelToView(pos);
@@ -167,11 +194,16 @@ public class ConsoleGUI extends JFrame {
 				if (e.getKeyCode() == KeyEvent.VK_QUOTE) {
 					if (inp.getText().length() > inp.getCaretPosition()
 							&& inp.getText().charAt(inp.getCaretPosition()) == '"') {
-						inp.setText(inp.getText().substring(0, inp.getText().length() - 1));
-						inp.setCaretPosition(inp.getCaretPosition());
+						// if caret is not at end of string && caret is before quote ("dog|")
+						int caret = inp.getCaretPosition();
+						inp.setText(inp.getText().substring(0, caret)+inp.getText().substring(caret+1, inp.getText().length()));
+						inp.setCaretPosition(caret);
 					} else {
-						inp.setText(inp.getText() + "\"");
-						inp.setCaretPosition(inp.getCaretPosition() - 1);
+						// insert quote after caret
+						int caret = inp.getCaretPosition();
+						inp.setText(inp.getText().substring(0, inp.getCaretPosition()) + "\""
+								+ inp.getText().substring(inp.getCaretPosition(), inp.getText().length()));
+						inp.setCaretPosition(caret);
 					}
 				} else if (e.getKeyCode() == KeyEvent.VK_UP) {
 					if (command_index <= ci.getPreviousSize() - 1)
@@ -188,26 +220,40 @@ public class ConsoleGUI extends JFrame {
 				}
 			}
 		});
-		inp.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String input = inp.getText();
-				write(input);
-				// append(cons,input+"\n",font_color);
-				inp.setText("");
-
-				// process
-				ci.parse(input.trim());
-
-				append(cons, ConsoleInput.begin, ConsoleInput.begin_color);
-			}
-		});
+		inp.addActionListener(e -> input(inp.getText()));
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.weightx = 0.5;
 		c.weighty = 0.0;
 		c.gridx = 0;
 		c.gridy = 1;
 		main.add(inp, c);
+
+		mbar = new JMenuBar();
+		run = new JMenu("Run");
+		open_and_run = new JMenuItem("Run .txt File", KeyEvent.VK_R);
+		open_and_run.setAccelerator(
+				KeyStroke.getKeyStroke(KeyEvent.VK_R, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
+		open_and_run.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				FileDialog fd = new FileDialog(getFrame(), "Select File", FileDialog.LOAD);
+				fd.setFilenameFilter((File dir, String name) -> name.toLowerCase().endsWith(".txt"));
+				fd.setVisible(true);
+				if (fd.getFile() == null)
+					return;
+
+				try (Stream<String> lines = Files.lines(Paths.get(fd.getFile()))) {
+					lines.forEachOrdered(s -> input(s));
+				} catch (IOException ex) {
+
+				}
+
+			}
+		});
+
+		run.add(open_and_run);
+		mbar.add(run);
+		setJMenuBar(mbar);
 
 		getContentPane().add(main);
 	}
@@ -216,6 +262,7 @@ public class ConsoleGUI extends JFrame {
 		if (System.getProperty("os.name").toLowerCase().contains("mac")) {
 			Application ma = Application.getApplication();
 			ma.setPreferencesHandler(arg0 -> buildPrefFrame());
+			ma.setAboutHandler(arg0 -> buildAboutFrame());
 		}
 	}
 
@@ -237,12 +284,6 @@ public class ConsoleGUI extends JFrame {
 				for (AbstractColorChooserPanel accp : panels) {
 					if (accp.getDisplayName().equals("RGB")) {
 						JOptionPane.showMessageDialog(null, accp);
-						accp.getColorSelectionModel().addChangeListener(new ChangeListener() {
-							@Override
-							public void stateChanged(ChangeEvent e) {
-								cons.setBackground(jcc.getColor());
-							}
-						});
 					}
 				}
 			}
@@ -255,6 +296,21 @@ public class ConsoleGUI extends JFrame {
 		pref.setMinimumSize(new Dimension(150, 150));
 		pref.pack();
 		pref.setVisible(true);
+	}
+
+	private void buildAboutFrame() {
+		JFrame about = new JFrame("About");
+		about.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		about.setLocationRelativeTo(getFrame());
+		JPanel panel = new JPanel();
+		JLabel about_txt = new JLabel("Console v." + getUID());
+
+		panel.add(about_txt);
+		about.setContentPane(panel);
+
+		about.setMinimumSize(new Dimension(150, 100));
+		about.pack();
+		about.setVisible(true);
 	}
 
 	public void write(String m) {
@@ -290,6 +346,19 @@ public class ConsoleGUI extends JFrame {
 		} catch (Exception e) {
 
 		}
+		JScrollBar vert = sp.getVerticalScrollBar();
+		vert.setValue(vert.getMaximum());
+	}
+
+	public void input(String input) {
+		write(input);
+		inp.setText("");
+
+		// process
+		ci.parse(input.trim());
+
+		if (!ci.run)
+			append(cons, ConsoleInput.begin, ConsoleInput.begin_color);
 	}
 
 	public void clear() {
@@ -308,6 +377,14 @@ public class ConsoleGUI extends JFrame {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
+				try {
+					System.setProperty("apple.laf.useScreenMenuBar", "true");
+					System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Test");
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+				} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+						| UnsupportedLookAndFeelException e) {
+					e.printStackTrace();
+				}
 				ConsoleGUI cg = new ConsoleGUI();
 				cg.inp.requestFocusInWindow();
 			}
